7073bafb9e81459a16ad6716c3beb37ae3e2571a
elisabichiappini <elisabichiappini@gmail.com>
2023-11-14 20:47:45 +0100

    feat: scaffolding pariedispari

---
+++ pariedispari/myscript.js
@@ -0,0 +1,17 @@
+//restizione linguaggio Js
+'use strict';
+
+// Pari e Dispari
+// 1 - Prompt a utente pari o dispari
+// 2 - Prompt a utente inserisce un numero da 1 a 5
+// 3 - Funzione genera numero random da 1 a 5 per computer.
+// 4 - Somma i due numeri.
+// 5 - Condizione: è una somma pari o dispari? SE la somma = pari && la scelta = pari |OR| la somma è dispari e la scelta è dispari allora vince utente*; ALTRIMENTI vince Computer
+
+/*
+FUNCTIONS
+*/
+
+/*
+OPERATIONS
+*/
