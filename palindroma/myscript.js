823bf153435f400de78098761d467ce2021c3847
elisabichiappini <elisabichiappini@gmail.com>
2023-11-14 21:48:00 +0100

    feat: palindroma function

--- palindroma/myscript.js
+++ palindroma/myscript.js
@@ -2,14 +2,31 @@
 'use strict';
 
 // Palidroma
-// - Prompt a utente inserisci una parola;
-// - Funzione acquisire valore parola; 
-// - Condizione: la parola è palindroma? SE palindroma true ALTRIMENTI false
+// 1 - Prompt a utente inserisci una parola;
+// 2 - Funzione acquisire valore parola; 
+// 3 - Condizione: la parola è palindroma? SE palindroma true ALTRIMENTI false
 
 /*
 FUNCTIONS
 */
+// 2 - Funzione acquisire valore parola; 
+function palindrogram (parola) {
+    const palindromaValidation = parola.toLowerCase();
+    if (palindromaValidation === palindromaValidation.split('').reverse().join('')) {
+        return true;
+    }
+    return false;
+}
 
 /*
 OPERATIONS
 */
+// 1 Prompt a utente inserisci una parola;
+const inpuUtente = prompt('Inserisci una parola');
+
+// 3 - Condizione: la parola è palindroma? SE palindroma true ALTRIMENTI false
+if (palindrogram(inpuUtente) === true) {
+    alert(`La parola ${inpuUtente} è palindroma.`)
+} else {
+    alert(`La parola ${inpuUtente} non è palindroma.`)
+}
