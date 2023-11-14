95362f05cb60e515dedc69507faad014a932bb79
elisabichiappini <elisabichiappini@gmail.com>
2023-11-14 20:47:22 +0100

    feat: scaffolding palindroma

---
+++ palindroma/myscript.js
@@ -0,0 +1,15 @@
+//restizione linguaggio Js
+'use strict';
+
+// Palidroma
+// - Prompt a utente inserisci una parola;
+// - Funzione acquisire valore parola; 
+// - Condizione: la parola è palindroma? SE palindroma true ALTRIMENTI false
+
+/*
+FUNCTIONS
+*/
+
+/*
+OPERATIONS
+*/
